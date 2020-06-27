0ccc7bf1b4858dadb6a44471d0abf20ed5b360c2
Ingo Hehner <ihehner@gmail.com>
2020-06-27 12:26:18 -0400

    Small change on user api test, forgot name

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -34,7 +34,8 @@ class PublicUserApiTests(TestCase):
 
     def test_user_exists(self):
         """Test creating a user that already exists"""
-        payload = {'email': 'blah@blah.com', 'password': 'test123'}
+        payload = {'email': 'blah@blah.com', 'password': 'test123',
+                   'name': 'Blah'}
         create_user(**payload)
 
         res = self.client.post(CREATE_USER_URL, payload)
@@ -43,7 +44,7 @@ class PublicUserApiTests(TestCase):
 
     def test_password_to_short(self):
         """Test to make sure error when password is to short"""
-        payload = {'email': 'blah@blah.com', 'password': 'pw'}
+        payload = {'email': 'blah@blah.com', 'password': 'pw', 'name': 'Blah'}
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
